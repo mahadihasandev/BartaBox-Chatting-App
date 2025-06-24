442fb838b0adb0e8e85fc444fe34c920a38089d2
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-06-24 23:59:03 +0600

    page crated

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}></Route>
+    <>
+    <Route path="/" element={<Login />}></Route>
+    <Route path='/registration' element={<Registration}></Route>
+    </>
   )
 );
 
