6b3ea638f8531b143e24e910e4148463b4c2d4b9
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-07-16 23:20:13 +0600

    Sideber start

--- src/layouts/RootLayouts.jsx
+++ src/layouts/RootLayouts.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Grid from '@mui/material/Grid';
+import SideBar from '../components/SideBar';
 
 function RootLayouts() {
   return (
     <>
-        <Grid container spacing={2}>
-  <Grid size={2}>
-    
-  </Grid>
-  <Grid size={10}>
-    <Outlet/>
-  </Grid>
+    <Grid container spacing={2}>
+      <Grid size={2}>
+        <SideBar/>
+      </Grid>
+      <Grid size={10}>
+        <Outlet/>
+      </Grid>
  
-</Grid>
+    </Grid>
         
     </>
   )
