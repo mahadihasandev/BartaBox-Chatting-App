803787f4340c1483af3c046b8248dda7eed0fc5f
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-07-18 13:44:48 +0600

    Serch end

--- src/layouts/UserList.jsx
+++ src/layouts/UserList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { LuSearch } from "react-icons/lu";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 function UserList() {
   return (
-    <div>UserList</div>
+    <>
+    <div className='user-box'>
+      <div className='user-search-box'>
+        <LuSearch className='search-icon'/>
+        <input placeholder='Search' type="text" />
+        <BsThreeDotsVertical className='threeDot-icon'/>
+      </div>
+      <div></div>
+    </div>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate} from 'react-router-dom'
 import { Bounce, toast, ToastContainer } from 'react-toastify';
+import UserList from '../layouts/UserList';
+import Grid from '@mui/material/Grid';
 
 function Home() {
   let navigate=useNavigate()
@@ -18,34 +20,33 @@ let data=useSelector((state)=>(state.activeUser.value)
 
   return (
     <>
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Bounce}
+    />
 
-      <Grid container spacing={2}>
-                           <ToastContainer
-              position="top-center"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={false}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="dark"
-              transition={Bounce}
-              />
-
-  <Grid size={4}>
-    
-  </Grid>
-  <Grid size={4}>
-  
-  </Grid>
-  <Grid size={4}>
-    
-  </Grid>
-  
-</Grid>
-
+  <div className='grid-division'>
+    <Grid container spacing={3}>
+      <Grid size={4}>
+        <UserList/>
+      </Grid>
+      <Grid size={4}>
+        <UserList/>
+      </Grid>
+      <Grid size={4}>
+       <UserList/>
+      </Grid>  
+    </Grid>
+  </div>
     </>
   )
 }
