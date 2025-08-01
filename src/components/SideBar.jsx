cfbc83b7aca1eb6d0efadbfa92df890d68bf6afb
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-01 19:18:11 +0600

    fire base storage

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,85 +1,88 @@
-import pPic from '../assets/ppic.png'
+
 import { TbHomeDown } from "react-icons/tb";
 import { AiOutlineMessage } from "react-icons/ai";
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { MdOutlineSettings } from "react-icons/md";
 import { HiOutlineLogout } from "react-icons/hi";
-import { Link, useLocation } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux'
-import React, { createRef, useEffect, useRef, useState } from 'react'
-import { useNavigate} from 'react-router-dom'
-import { userDetails } from '../slices/userInfoSlice';
-import { Bounce, toast, ToastContainer } from 'react-toastify';
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import React, { createRef, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userDetails } from "../slices/userInfoSlice";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 import { getAuth, signOut } from "firebase/auth";
 import { FaCloudUploadAlt } from "react-icons/fa";
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
 
+// import { getStorage, ref, uploadString, getDownloadURL } from "firebase/storage";
 
 const BootstrapButton = styled(Button)({
-  width:'35%',
-  padding:"19px 0px",
-  background:"#5F35F5",
-  fontFamily:"Open Sans",
-  borderRadius:"30px",
-  
-  
+  width: "35%",
+  padding: "19px 0px",
+  background: "#5F35F5",
+  fontFamily: "Open Sans",
+  borderRadius: "30px",
 });
 
 function SideBar() {
-  const [Locations,setLocation]=useState();
-  const [visiblePopup,setVisiblePopup]=useState(false)
-   const auth = getAuth();
-  let dispatch=useDispatch()
-  let navigate=useNavigate()
-  let location=useLocation()
+  const [Locations, setLocation] = useState();
+  const [visiblePopup, setVisiblePopup] = useState(false);
+
+  // const storage = getStorage();
+
+  const auth = getAuth();
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+  let location = useLocation();
   const [image, setImage] = useState("");
 
   const [cropData, setCropData] = useState("");
   const cropperRef = createRef();
-  console.log(cropData);
   
-  useEffect(()=>{
-    setLocation(location.pathname.replace("/pages/",""));
-  },)
-  let domRef=useRef(null)
+  
 
-let data=useSelector((state)=>(state.activeUser.value))
-
-  useEffect(
-    ()=>{
-      if(!data){
-          navigate('/login')
-      }
-    },[])
-
-let handleLogOut=()=>{
-  signOut(auth).then(() => {
-    localStorage.removeItem("activeUser")
-    dispatch(userDetails(null))
-    navigate('/login')
-  }).catch((error) => {
-      let errorCode=error.code
-      toast.error(errorCode)
+  useEffect(() => {
+    setLocation(location.pathname.replace("/pages/", ""));
   });
-}
+  let domRef = useRef(null);
 
-let handleUpdateProfile=()=>{
-  setVisiblePopup(true)
-}
+  let data = useSelector((state) => state.activeUser.value);
+  let ppData=data.photoURL
+  // let imgdata=useSelector((state)=>console.log(state.activeUser.value.photoURL))
 
-let handlePopUp=(e)=>{
-  
-  if(!domRef.current.contains(e.target)){    
-    setVisiblePopup(false)
-  }
-}
+  useEffect(() => {
+    if (!data) {
+      navigate("/login");
+    }
+  }, []);
+
+  let handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem("activeUser");
+        dispatch(userDetails(null));
+        navigate("/login");
+      })
+      .catch((error) => {
+        let errorCode = error.code;
+        toast.error(errorCode);
+      });
+  };
+
+  let handleUpdateProfile = () => {
+    setVisiblePopup(true);
+  };
+
+  let handlePopUp = (e) => {
+    if (!domRef.current.contains(e.target)) {
+      setVisiblePopup(false);
+    }
+  };
 
   const onChange = (e) => {
-    
-    
     e.preventDefault();
     let files;
     if (e.dataTransfer) {
@@ -97,76 +100,97 @@ let handlePopUp=(e)=>{
   const getCropData = () => {
     if (typeof cropperRef.current?.cropper !== "undefined") {
       setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+      // const storageRef = ref(storage, auth.currentUser.uid);
+
+    //   const message4 = cropData;
+    //   uploadString(storageRef, message4, "data_url").then((snapshot) => {
+    //     console.log("Uploaded a data_url string!");
+    //   });
+      // getDownloadURL(storageRef).then((downloadURL) => {
+      //   console.log("File available at", downloadURL);
+      // });
     }
   };
 
   return (
     <>
-        <div className='sidebar-layouts'>
-          <div onClick={handleUpdateProfile} className='profile-layout'>
-            <img src={pPic} alt="Img" />
-            <div className="overlay">
-              <FaCloudUploadAlt className='icon'/>
-            </div>
+      <div className="sidebar-layouts">
+        <div onClick={handleUpdateProfile} className="profile-layout">
+          <img src={ppData} alt="Img" />
+          <div className="overlay">
+            <FaCloudUploadAlt className="icon" />
           </div>
-          <div className='page-layout'>
-            <Link className={Locations=="home"&&"active"} to='/pages/home'>
-          <TbHomeDown className='page-icon'/>
+        </div>
+        <div className="page-layout">
+          <Link className={Locations == "home" && "active"} to="/pages/home">
+            <TbHomeDown className="page-icon" />
           </Link>
-            <Link className={Locations=="messages"&&"active"} to='/pages/messages'>
-            <AiOutlineMessage className='page-icon'/>
-            </Link>
-            <Link className={Locations=="notification"&&"active"} to='/pages/notification'>
-            <MdOutlineNotificationsActive className='page-icon'/>
-            </Link>
-            <Link className={Locations=="settings"&&"active"} to='/pages/settings'>
-            <MdOutlineSettings className='page-icon'/>
-            </Link>
-          </div>
-          <div className='logout-layout'>
-              <HiOutlineLogout onClick={handleLogOut} className='page-icon'/>
-          </div>
+          <Link
+            className={Locations == "messages" && "active"}
+            to="/pages/messages"
+          >
+            <AiOutlineMessage className="page-icon" />
+          </Link>
+          <Link
+            className={Locations == "notification" && "active"}
+            to="/pages/notification"
+          >
+            <MdOutlineNotificationsActive className="page-icon" />
+          </Link>
+          <Link
+            className={Locations == "settings" && "active"}
+            to="/pages/settings"
+          >
+            <MdOutlineSettings className="page-icon" />
+          </Link>
+        </div>
+        <div className="logout-layout">
+          <HiOutlineLogout onClick={handleLogOut} className="page-icon" />
+        </div>
 
-          {visiblePopup&&<div onClick={handlePopUp} className='popup-image'>
+        {visiblePopup && (
+          <div onClick={handlePopUp} className="popup-image">
             <div ref={domRef} className="popup-img-box">
               <h2>Change your profile picture</h2>
               <input onChange={onChange} type="file" />
-              <div style={{display:"flex"}}>
-            <div>
-          {image&& <Cropper
-          ref={cropperRef}
-          style={{ height: 400, width: "100%" }}
-          initialAspectRatio={1}
-          preview=".img-preview"
-          src={image}
-          viewMode={1}
-          minCropBoxHeight={10}
-          minCropBoxWidth={10}
-          background={false}
-          responsive={true}
-          autoCropArea={1}
-          checkOrientation={false}
-          guides={true}
-        />}
+              <div style={{ display: "flex" }}>
+                <div>
+                  {image && (
+                    <Cropper
+                      ref={cropperRef}
+                      style={{ height: 400, width: "100%" }}
+                      initialAspectRatio={1}
+                      preview=".img-preview"
+                      src={image}
+                      viewMode={1}
+                      minCropBoxHeight={10}
+                      minCropBoxWidth={10}
+                      background={false}
+                      responsive={true}
+                      autoCropArea={1}
+                      checkOrientation={false}
+                      guides={true}
+                    />
+                  )}
+                </div>
+                <div>
+                  <div className="preview-box">
+                    <h1>Preview</h1>
+                    <div className="img-preview preview-round"></div>
+                  </div>
+                </div>
               </div>
               <div>
-          <div className="preview-box">
-          <h1>Preview</h1>
-          <div className="img-preview preview-round"></div>
-        </div>
+                <BootstrapButton onClick={getCropData} variant="contained">
+                  Upload
+                </BootstrapButton>
               </div>
-              </div>
-              <div>
-                <BootstrapButton  onClick={getCropData} variant="contained">Upload</BootstrapButton>
-              </div>
-               
             </div>
-            
-          </div>}
-
-        </div>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,137 +1,136 @@
-import React, { useState } from 'react'
-import Grid from '@mui/material/Grid';
-import { getAuth, createUserWithEmailAndPassword ,sendEmailVerification } from "firebase/auth";
-import TextField from '@mui/material/TextField';
-import {styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import { ToastContainer, toast,Bounce } from 'react-toastify';
-import RegistrationImg from '../assets/registration.png';
-import { Link, useNavigate } from 'react-router-dom';
-import { FiEye,FiEyeOff } from "react-icons/fi";
-import fireBaseConfig from '../FirebaseConfig';
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import RegistrationImg from "../assets/registration.png";
+import { Link, useNavigate } from "react-router-dom";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import fireBaseConfig from "../FirebaseConfig";
 
 const BootstrapButton = styled(Button)({
-  width:'55%',
-  padding:"19px 0px",
-  background:"#5F35F5",
-  borderRadius:"86px",
-  fontFamily:"Open Sans",
+  width: "55%",
+  padding: "19px 0px",
+  background: "#5F35F5",
+  borderRadius: "86px",
+  fontFamily: "Open Sans",
 });
-
 
 const CssTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: '#11175D',
+  "& label.Mui-focused": {
+    color: "#11175D",
   },
-   '& .MuiInput-underline:after': {
-    borderBottomColor: '#B2BAC2',
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
   },
-  '& .MuiOutlinedInput-root': {
-    
-    '&.Mui-focused fieldset': {
-      borderColor: '#11175D',
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "#11175D",
     },
   },
-  width:"55%",
-  paddingBottom:"33px"
+  width: "55%",
+  paddingBottom: "33px",
 });
 
-
 function Registration() {
-
-  const [showPass,setShowPass]=useState(false);
-  const [email,setEmail]=useState("");
-  const [emailError,setEmailError]=useState("");
-  const [nameError,setNameError]=useState("");
-  const [passError,setPassError]=useState("");
-  const [name,setName]=useState("");
-  const [pass,setPass]=useState("");
-  let navigate=useNavigate()
+  const [showPass, setShowPass] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [passError, setPassError] = useState("");
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  let navigate = useNavigate();
   const auth = getAuth(fireBaseConfig);
-    
 
-    function handleEmail(e){
-      
-        setEmail(e.target.value)
-        setEmailError("")
+  function handleEmail(e) {
+    setEmail(e.target.value);
+    setEmailError("");
+  }
 
-    }
+  function handleName(e) {
+    setName(e.target.value);
+    setNameError("");
+  }
 
-    function handleName(e){
-        setName(e.target.value)
-        setNameError("")
-    }
+  function handlePass(e) {
+    setPass(e.target.value);
+    setPassError("");
+  }
 
-    function handlePass(e){
-        setPass(e.target.value)
-        setPassError("")
-    }
+  let handleEyeClick = () => {
+    setShowPass(!showPass);
+  };
 
-     let handleEyeClick=()=>{     
-      setShowPass(!showPass)      
-    }
-
-   function handleClick(){
+  function handleClick() {
     let hasError = false;
 
-    if(!email){       
-        setEmailError("Email does not exist")
-        hasError = true;        
-      }else{
-        if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
-      setEmailError("Type a valid email")
+    if (!email) {
+      setEmailError("Email does not exist");
       hasError = true;
-    }}
+    } else {
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        setEmailError("Type a valid email");
+        hasError = true;
+      }
+    }
 
-
-    if(!name){
-      setNameError("name is required")
+    if (!name) {
+      setNameError("name is required");
       hasError = true;
-    }else if(!/^.{2,15}$/.test(name)){
-      setNameError("Name must be between 2-16 character long")
-       hasError = true;
-    }else if(!/^[a-zA-Z].*$/.test(name)){
-      setNameError("Must be start with a letter") 
+    } else if (!/^.{2,15}$/.test(name)) {
+      setNameError("Name must be between 2-16 character long");
       hasError = true;
-    }else if(!/^[a-zA-Z0-9_]+$/.test(name)){
-      setNameError("only later number & underscore are valid")
+    } else if (!/^[a-zA-Z].*$/.test(name)) {
+      setNameError("Must be start with a letter");
+      hasError = true;
+    } else if (!/^[a-zA-Z0-9_]+$/.test(name)) {
+      setNameError("only later number & underscore are valid");
       hasError = true;
     }
 
-    if(!pass){
-      setPassError("Password is empty")
+    if (!pass) {
+      setPassError("Password is empty");
       hasError = true;
-    }else if(!/^.{8,}$/.test(pass)){
-        setPassError("at least 8 character")
-        hasError = true;
-      }else if(!/.*[A-Z]/.test(pass)){
-      setPassError("at least one upper case")
+    } else if (!/^.{8,}$/.test(pass)) {
+      setPassError("at least 8 character");
       hasError = true;
-    }else if (!/^.*[a-z]/.test(pass)){
-      setPassError("at least one lower case")
+    } else if (!/.*[A-Z]/.test(pass)) {
+      setPassError("at least one upper case");
       hasError = true;
-    }else if (!/.*\d/.test(pass)){
-      setPassError("at least one number")
+    } else if (!/^.*[a-z]/.test(pass)) {
+      setPassError("at least one lower case");
+      hasError = true;
+    } else if (!/.*\d/.test(pass)) {
+      setPassError("at least one number");
       hasError = true;
       // (?=.*[@$!%*?&])
       // [A-Za-z\d@$!%*?&]{8,}$
     }
 
-  //    if(email&&!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)&&pass&&name){
-  //     console.log(email+name+pass)
-  //   }else{
-  //     console.log("error")
-  //   }
-  //  }
+    //    if(email&&!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)&&pass&&name){
+    //     console.log(email+name+pass)
+    //   }else{
+    //     console.log("error")
+    //   }
+    //  }
 
-
-    if(!hasError){
-        
-        createUserWithEmailAndPassword(auth, email, pass)
+    if (!hasError) {
+      createUserWithEmailAndPassword(auth, email, pass)
         .then((user) => {
+          updateProfile(auth.currentUser, {
+            displayName: "arnob",
+            photoURL:"./src/assets/ppic.png",
           
-          sendEmailVerification(auth.currentUser)
-          .then(() => {
+          }).then(() => {
+             sendEmailVerification(auth.currentUser)
             toast.success("Email Verification send");
             setEmail('')
             setName('')
@@ -139,69 +138,93 @@ function Registration() {
             toast.success("New Account Created")
             setTimeout(()=>{
             navigate("/login")
-            },2000)
-            });
-        
+            },2000) 
+             
+            
+            })
+            
+        })
+        .catch((error) => {
+          const errorCode = error.code;
 
-  })
-  .catch((error) => {
-    
-    const errorCode=error.code;
-   
-    toast.error(errorCode)
-      setEmail('')
-      setName('')
-      setPass('')
-  
-  });
-  }}
-   
+          toast.error(errorCode);
+          setEmail("");
+          setName("");
+          setPass("");
+        });
+    }
+  }
+
   return (
     <>
       <Grid container>
-        <Grid size={{xs:12,md:6}} spacing={20}>
-         <div className='reg-box'>
+        <Grid size={{ xs: 12, md: 6 }} spacing={20}>
+          <div className="reg-box">
             <ToastContainer
-               position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"
-               transition={Bounce}
+              position="top-center"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="dark"
+              transition={Bounce}
             />
-           <div className='reg-title'>
-            <h2>Get started with easily register</h2>
-            <p>Free register and you can enjoy it</p>
-            {emailError&&<div className='error-screen'>{emailError}</div>}
-            <CssTextField value={email} onChange={handleEmail} id="outlined-basic" label="Email Address" variant="outlined" />
-            {nameError&&<div className='error-screen'>{nameError}</div>}
-            <CssTextField value={name} onChange={handleName} id="outlined-basic" label="Full name" variant="outlined" />
-            {passError&&<div className='error-screen'>{passError}</div>}
-            <div className='passField'>
-              <CssTextField value={pass} onChange={handlePass} type={showPass?"text":"password"} id="outlined-basic" label="Password" variant="outlined" />
-              <div onClick={handleEyeClick} className='fa-eye-on'>                          
-                {showPass?<FiEye />:<FiEyeOff />}
+            <div className="reg-title">
+              <h2>Get started with easily register</h2>
+              <p>Free register and you can enjoy it</p>
+              {emailError && <div className="error-screen">{emailError}</div>}
+              <CssTextField
+                value={email}
+                onChange={handleEmail}
+                id="outlined-basic"
+                label="Email Address"
+                variant="outlined"
+              />
+              {nameError && <div className="error-screen">{nameError}</div>}
+              <CssTextField
+                value={name}
+                onChange={handleName}
+                id="outlined-basic"
+                label="Full name"
+                variant="outlined"
+              />
+              {passError && <div className="error-screen">{passError}</div>}
+              <div className="passField">
+                <CssTextField
+                  value={pass}
+                  onChange={handlePass}
+                  type={showPass ? "text" : "password"}
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                />
+                <div onClick={handleEyeClick} className="fa-eye-on">
+                  {showPass ? <FiEye /> : <FiEyeOff />}
+                </div>
               </div>
-            </div>
 
-            <BootstrapButton onClick={handleClick} variant="contained">Sign up</BootstrapButton>
-            <p>Already  have an account ? <Link to='/login'><span>Sign In</span></Link> </p>
+              <BootstrapButton onClick={handleClick} variant="contained">
+                Sign up
+              </BootstrapButton>
+              <p>
+                Already have an account ?{" "}
+                <Link to="/login">
+                  <span>Sign In</span>
+                </Link>{" "}
+              </p>
+            </div>
           </div>
-         </div>
-           
-  
         </Grid>
-        <Grid className='reg-grid' size={{xs:0,md:6}}>
-          <img className='regImg' src={RegistrationImg} alt="Image" />
-        </Grid>  
+        <Grid className="reg-grid" size={{ xs: 0, md: 6 }}>
+          <img className="regImg" src={RegistrationImg} alt="Image" />
+        </Grid>
       </Grid>
     </>
-  )
+  );
 }
 
-export default Registration
+export default Registration;
