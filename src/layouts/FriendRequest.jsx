7f2719b2cae8ddc8b070f3c6e31a995de144368f
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-08 09:43:42 +0600

    Accept frrq data sending to firebase

--- src/layouts/FriendRequest.jsx
+++ src/layouts/FriendRequest.jsx
@@ -1,74 +1,87 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { LuSearch } from "react-icons/lu";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import SingleUser from '../components/SingleUser';
-import { getDatabase, ref, onValue } from "firebase/database";
-import { useSelector } from 'react-redux';
+import SingleUser from "../components/SingleUser";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import { useSelector } from "react-redux";
 
 function FriendRequest() {
-const db = getDatabase();
-let [friendReq,setFriendReq]=useState([])
+  const db = getDatabase();
+  let [friendReq, setFriendReq] = useState([]);
 
+  let data = useSelector((state) => state.activeUser.value);
+  useEffect(() => {
+    const starCountRef = ref(db, "friendRequest/");
+    onValue(starCountRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().receiverId == data.uid) {
+          arr.push({ ...item.val() });
+        }
+      });
+      setFriendReq(arr);
+    });
+  }, []);
 
-let data=useSelector((state)=>(state.activeUser.value))
-  useEffect(()=>{
-    
-    const starCountRef = ref(db, 'friendRequest/');
-onValue(starCountRef, (snapshot) => {
-  let arr=[]
-  snapshot.forEach((item)=>{
-    if(item.val().receiverId==data.uid){
-        arr.push({...item.val()})
-    }
-    
-  })
-  setFriendReq(arr);
-});
-  },[])
+  let handleAccept = (item) => {
+    console.log(item);
+    set(ref(db, 'friendList/'), {
+      senderId: item.senderId,
+      senderName: item.senderName,
+      photo : item.photo,
+    });
+  };
 
-let handleAccept=(item)=>{
-  console.log(item);
-  
-}
-  
+  // photo: "https://lh3.googleusercontent.com/a/ACg8ocInBlZnxXjU-ylKDBfuYeM7q3ZF9DhMlj1m2e3XVUV9UlheGLJpgg=s96-c";
+  // receiverId: "fjSk7TsQHOQVAZe1ETNaSEVdg092";
+  // receiverName: "arnob hasan";
+  // senderId: "6OsNwRKKJhWGJlKUCzsOJMDNDr42";
+  // senderName: "Arnob Hasan";
+
   return (
     <>
-    <div className='user-box'>
-      <div className='user-search-box'>
-        <LuSearch className='search-icon'/>
-        <input placeholder='Search' type="text" />
-        <BsThreeDotsVertical className='threeDot-icon'/>
-      </div>
-      <div className="userList-box">
-        <div className='userList-title-box '>
-          <h4 className='userList-title'>Friend Request</h4>
-          <BsThreeDotsVertical className='userList-threeDot'/>
+      <div className="user-box">
+        <div className="user-search-box">
+          <LuSearch className="search-icon" />
+          <input placeholder="Search" type="text" />
+          <BsThreeDotsVertical className="threeDot-icon" />
         </div>
-          
-       <>
-       {
-        friendReq.map((item)=>(
-            <div className='profile-box'>
-        <div className='profile-img-title-box'>
-          <div className='profile-img-box'>
-          <img className='profile-img' src={item.photo} alt="Profile-image" />
+        <div className="userList-box">
+          <div className="userList-title-box ">
+            <h4 className="userList-title">Friend Request</h4>
+            <BsThreeDotsVertical className="userList-threeDot" />
           </div>
-        <div className='profile-title'>
-          <h4>{item.senderName}</h4>
-          <p>Hi Guys, Wassup!</p>
-        </div>
-        </div>
-        <button onClick={()=>{handleAccept(item)}}>Accept</button>
-      </div>
-        ))
-       }
-        
-       </>
 
-      </div>     
-    </div>
+          <>
+            {friendReq.map((item) => (
+              <div className="profile-box">
+                <div className="profile-img-title-box">
+                  <div className="profile-img-box">
+                    <img
+                      className="profile-img"
+                      src={item.photo}
+                      alt="Profile-image"
+                    />
+                  </div>
+                  <div className="profile-title">
+                    <h4>{item.senderName}</h4>
+                    <p>Hi Guys, Wassup!</p>
+                  </div>
+                </div>
+                <button
+                  onClick={() => {
+                    handleAccept(item);
+                  }}
+                >
+                  Accept
+                </button>
+              </div>
+            ))}
+          </>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default FriendRequest
+export default FriendRequest;
