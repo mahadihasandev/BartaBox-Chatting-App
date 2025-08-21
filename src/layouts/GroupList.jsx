e5142dc36b2f95e935a5bcdf817a3c485e377af2
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-21 09:36:09 +0600

    group list implemented

--- src/layouts/GroupList.jsx
+++ src/layouts/GroupList.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { LuSearch } from "react-icons/lu";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import SingleUser from '../components/SingleUser';
+import { getDatabase, onValue, ref } from 'firebase/database';
 
 function GroupList() {
+
+const [allGroupdata, setAllGroupdata]=useState([])
+const db = getDatabase();
+//reding group data from firebase
+
+  useEffect(()=>{
+const starCountRef = ref(db, 'Mygroup/');
+let array = [];
+onValue(starCountRef, (snapshot) => {
+  snapshot.forEach((item)=>{
+      array.push(item.val());
+      setAllGroupdata(array);
+  })
+});
+  },[])
+
+
   return (
     <>
     <div className='user-box'>
@@ -18,11 +36,34 @@ function GroupList() {
           <BsThreeDotsVertical className='userList-threeDot'/>
         </div>
           
-        <SingleUser/>
-        <SingleUser/>
-        <SingleUser/>
-        <SingleUser/>
-        <SingleUser/>
+       {
+          allGroupdata.map((item)=>(
+            
+            
+            <>
+             <div className="profile-box">
+                <div className="profile-img-title-box">
+                  <div className="profile-img-box">
+                    <img
+                      className="profile-img"
+                      src={item.photo}
+                      alt="Profile-image"
+                    />
+                  </div>
+                  <div className="profile-title">
+                    <h4>{item.groupName}</h4>
+                     <h4>{item.adminName}</h4>
+                    <p>Hi Guys, Wassup!</p>
+                  </div>
+                </div>
+                <button>
+                  Accept
+                </button>
+              </div>
+            </>
+            
+          ))
+        }
 
 
 
